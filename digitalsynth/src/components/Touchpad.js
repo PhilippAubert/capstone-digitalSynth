6a80691fe6e48b4bb093d72dd4b55c21b7bf8dca
Philipp Aubert <p.b.m.aubert@gmail.com>
2021-05-02 16:56:52 +0200

    Add Touchpad

--- digitalsynth/src/components/Touchpad.js
+++ digitalsynth/src/components/Touchpad.js
@@ -1,5 +1,21 @@
+import { useState } from "react";
 import "./css/Touchpad.css";
 
 export default function Touchpad() {
-  return <div className="Touchpad"></div>;
+  const [pointPos, setPointPos] = useState({ x: 110, y: 90 });
+
+  function handlePointerMove(event) {
+    const { clientX, clientY } = event;
+    setPointPos({ x: clientX, y: clientY });
+  }
+
+  return (
+    <div className="Touchpad" onPointerMove={handlePointerMove}>
+      {pointPos.x},{pointPos.y}
+      <div
+        className={`absolute box`}
+        style={{ top: `${pointPos.y}px`, left: `${pointPos.x}px` }}
+      ></div>
+    </div>
+  );
 }
