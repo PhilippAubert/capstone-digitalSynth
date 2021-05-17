6a9f0dbdf3fc3636d150855953ad12b693430768
Philipp Aubert <p.b.m.aubert@gmail.com>
2021-05-17 11:21:16 +0200

    Add dummy modal for Load

--- src/components/modals/Load.js
+++ src/components/modals/Load.js
@@ -1,12 +1,36 @@
-export default function Load() {
-  return (
+import ReactDOM from "react-dom";
+
+const MODAL_STYLES = {
+  position: "fixed",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  backgroundColor: "#FFF",
+  padding: "100px",
+  zIndex: 1000,
+};
+
+const OVERLAY_STYLES = {
+  position: "fixed",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: "rgba(0,0,0,.9)",
+  zIndex: 1000,
+};
+
+export default function Load({ open, children, onClose }) {
+  if (!open) return null;
+  return ReactDOM.createPortal(
     <div>
-      <h2>LOAD</h2>
-      <label>Load A Sound</label>
-      <ul>
-        <li> Pad Eerie</li>
-        <li> Hancock Bass</li>
-      </ul>
-    </div>
+      <div style={OVERLAY_STYLES}>
+        <div style={MODAL_STYLES}>
+          <button onClick={onClose}> Load Sound </button>
+          {children}
+        </div>
+      </div>
+    </div>,
+    document.getElementById("portal")
   );
 }
