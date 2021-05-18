29a1229a326aa88f3e2a4f201b8941cb5a32511d
Philipp Aubert <p.b.m.aubert@gmail.com>
2021-05-18 18:19:11 +0200

    Add Function-Board for Amp and make it fit

--- src/components/Amp.js
+++ src/components/Amp.js
@@ -10,32 +10,29 @@ export default function Amp({ attack, decay, onChangeAttack, onChangeDecay }) {
   }
 
   return (
-    <div>
-      {" "}
-      <div className="Function-Board">
-        <div className="Amp-bar">
-          <h2>SET AMP ENVELOPE</h2>
-        </div>{" "}
-        <label className="Amp-label"> Attack </label>
-        <input
-          value={attack}
-          onChange={handleAttackChange}
-          type="range"
-          min="0.1"
-          max="1000"
-          className="Value"
-          step="0.1"
-        />
-        <label className="Amp-label"> Decay </label>
-        <input
-          value={decay}
-          onChange={handleDecayChange}
-          type="range"
-          min="0"
-          max="1000"
-          className="Value"
-        />
-      </div>
+    <div className="Function-Board">
+      <div className="Amp-bar">
+        <h2>SET AMP ENVELOPE</h2>
+      </div>{" "}
+      <label className="Amp-label"> ATTACK</label>
+      <input
+        value={attack}
+        onChange={handleAttackChange}
+        type="range"
+        min="0.1"
+        max="1000"
+        className="Value"
+        step="0.1"
+      />
+      <label className="Amp-label"> DECAY</label>
+      <input
+        value={decay}
+        onChange={handleDecayChange}
+        type="range"
+        min="0"
+        max="1000"
+        className="Value"
+      />
     </div>
   );
 }
